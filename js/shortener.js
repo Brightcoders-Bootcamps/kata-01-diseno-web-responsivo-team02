e1abfe393f407e241ab71bf020f0db29849aed69
Andrés Ubilla <andres.ubilla@gmail.com>
2020-12-07 14:19:31 -0300

    merge.

--- js/shortener.js
+++ js/shortener.js
@@ -1,59 +1,15 @@
-//const fetch = require("node-fetch");
-const URL_SERVICE = 'https://cutt.ly/api/api.php?key=3fc4586da4f404ec2b2b63028ba72699f6403&short=';
-
-//shortenerUrl('http://andresubilla.com')
+const URL_SERVICE = 'https://api.shrtco.de/v2/shorten?url=';
 
 /* funcion que acorta una url dada */
 function shortenerUrl(url) {
+   console.log('call');
    if (isValidUrl(url)) {
-      $.ajax({
-         crossDomain: true,
-         crossOrigin: true,
-         type: "GET",
-         url: URL_SERVICE+url,
-         dataType: 'json',
-         accepts: "application/json",
-         headers: {
-            "Access-Control-Allow-Origin":"*",
-            "Access-Control-Allow-Credentials":true,
-            "Access-Control-Allow-Methods":"GET",
-            "Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept",
-            'Content-Type': 'application/javascript'
-         },
-         success: function(response) {
-            var jsonData = JSON.parse(response);
-            console.log(jsonData)
-         },
-         error: function() {
-            console.log('There was some error performing the AJAX call!');
-         }
-      })
-      /*
-      fetch(URL_SERVICE+url, {
-         'mode': 'no-cors',
-         'headers': {
-            'Access-Control-Allow-Origin': '*',
-            "Access-Control-Allow-Credentials" : true
-         }
-      })
-      .then((response) => {
-         return response.text();
-      })
-      .then(function(json) {
-         console.log(JSON.parse(json))
-         if (json.status < 7) {
-            console.log("ERROR EN EL SERVICIO: " + json.status)
-         } else {
-            //console.log("URL SHORT: " + json.url.shortLink)
-            //TODO guardar la url en algun lado para persistencia
-            //TODO set respuesta en un textbox/label en el template
-            //saveDataLocalStorage(json.shortLink);
-         }
-      })
-       */
+      axios.get(URL_SERVICE + url)
+         .then(response => console.log(response))
+         .catch(error => console.log(error))
    } else {
       console.log("NO ES UNA URL VALIDA");
-      //alert("NO ES UNA URL VALIDA");
+      alert("NO ES UNA URL VALIDA");
    }
 }
 
@@ -69,19 +25,19 @@ function copyUrlShort() {
 
 /* funcion para validar url */
 function isValidUrl(url) {
-   var regexp = new RegExp('^(https?:\\/\\/)?'+ // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+   var regexp = new RegExp('^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
 
    return !!regexp.test(url);
 }
 
 function saveDataLocalStorage(data) {
    if (window.localStorage) {
-      localStorage.setItem("link",data);
+      localStorage.setItem("link", data);
    } else {
       console.log("NO TIENE LOCAL STORAGE");
    }
