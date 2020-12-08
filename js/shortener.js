8ef776315665d6cd20a1cf8945bb479538024877
Andrés Ubilla <andres.ubilla@gmail.com>
2020-12-08 00:38:57 -0300

    Se integran funciones logicas en javascript.

--- js/shortener.js
+++ js/shortener.js
@@ -1,30 +1,61 @@
 const URL_SERVICE = 'https://api.shrtco.de/v2/shorten?url=';
 
 /* funcion que acorta una url dada */
-function shortenerUrl(url) {
-   console.log('call');
-   if (isValidUrl(url)) {
-      axios.get(URL_SERVICE + url)
-         .then(response => console.log(response))
-         .catch(error => console.log(error))
+function shortenerUrl() {
+   let url = $("#linkToShort").val();
+
+   if (url != '') {
+      console.log('call: ' + url);
+
+      if (isValidUrl(url)) {
+         axios.get(URL_SERVICE + url)
+            .then(function (response) {
+               console.log(response.data)
+               if (response.data.ok) {
+                  let html = $("#linksShortendedList").html();
+                  html += '<div class="shortened-links-list-item">' +
+                     '<div class="origin-link">'+response.data.result.original_link+'</div>' +
+                        '<div class="short-link">' +
+                           '<input id="'+response.data.result.code+'" class="short-link-hidden" value="'+response.data.result.short_link+'"><a href="'+response.data.result.short_link+'" title="'+response.data.result.short_link+'" target="_blank">'+response.data.result.short_link+'</a>' +
+                        '</div>' +
+                        '<div class="copy-link"><a href="#"><span class="" onClick="copyUrlShort(\''+response.data.result.code+'\')">Copy</span></a></div>' +
+                     '</div>';
+
+                  $("#linksShortendedList").html(html).fadeIn('fast');
+
+                  //guardo en local storage
+                  saveDataLocalStorage(response.data.result);
+               } else {
+                  console.log("HA OCURRIDO UN ERROR EN EL WS");
+                  alert("HA OCURRIDO UN ERROR EN EL WS");
+               }
+            })
+            .catch(error => console.log(error))
+      } else {
+         console.log("NO ES UNA URL VALIDA");
+         alert("NO ES UNA URL VALIDA");
+      }
    } else {
-      console.log("NO ES UNA URL VALIDA");
-      alert("NO ES UNA URL VALIDA");
+      console.log("DEBE INGRESAR UNA URL");
+      alert("DEBE INGRESAR UNA URL");
    }
 }
 
 /* funcion que copia la url corta en un link */
-function copyUrlShort() {
-   var copyText = document.getElementById("myInput");                         //SET REAL NAME INPUT
+function copyUrlShort(code) {
+   console.log("code: " + code);
+
+   var copyText = document.getElementById(code);                                       //SET REAL NAME INPUT
    copyText.select();
    copyText.setSelectionRange(0, 99999);                                     //For mobile devices
    document.execCommand("copy");
    console.log("texto copiado: " + copyText.value);
-   //alert("texto copiado: " + copyText.value);
+   alert("texto copiado: " + copyText.value);
 }
 
 /* funcion para validar url */
 function isValidUrl(url) {
+   console.log('url: ' + url);
    var regexp = new RegExp('^(https?:\\/\\/)?' + // protocol
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
       '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
@@ -37,8 +68,46 @@ function isValidUrl(url) {
 
 function saveDataLocalStorage(data) {
    if (window.localStorage) {
-      localStorage.setItem("link", data);
+      let obj = localStorage.getItem("listUrlShortened");
+      let objTmp;
+      if (obj != null) {
+         objTmp = JSON.parse(obj);
+         objTmp["links"].push({ "code": data.code, "link": data.original_link, "short": data.short_link });
+      } else {
+         objTmp = { "links": [{ "code": data.code, "link": data.original_link, "short": data.short_link }]};
+      }
+
+      localStorage.setItem("listUrlShortened", JSON.stringify(objTmp));
    } else {
       console.log("NO TIENE LOCAL STORAGE");
+      return;
+   }
+}
+
+/* funcion que es llamada al cargar la pagina para cargar la data del local storage si existe */
+function getDataLocalStorage() {
+   if (window.localStorage) {
+      let obj = localStorage.getItem("listUrlShortened");
+      let objTmp;
+      if (obj != null) {
+         objTmp = JSON.parse(obj);
+         let html = "";
+         for (var i = 0; i < objTmp["links"].length; i++) {
+            html += '<div class="shortened-links-list-item">' +
+               '<div class="origin-link">'+objTmp["links"][i].link+'</div>' +
+               '<div class="short-link">' +
+               '<input id="'+objTmp["links"][i].code+'" class="short-link-hidden" value="'+objTmp["links"][i].short+'"><a href="'+objTmp["links"][i].short+'" title="'+objTmp["links"][i].short+'" target="_blank">'+objTmp["links"][i].short+'</a>' +
+               '</div>' +
+               '<div class="copy-link"><a href="#"><span class="" onClick="copyUrlShort(\''+objTmp["links"][i].code+'\')">Copy</span></a></div>' +
+               '</div>';
+         }
+
+         $("#linksShortendedList").html(html).fadeIn('fast');
+      } else {
+         return;
+      }
+   } else {
+      console.log("NO TIENE LOCAL STORAGE");
+      return;
    }
 }
